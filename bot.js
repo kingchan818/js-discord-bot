1f421d7841a73692a002b57ce882c89bdf773054
calvin chan <chancch70@gmail.com>
2021-06-12 22:16:10 +0800

    bug fixed

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 require('dotenv').config();
 const youtube_music = require('ytdl-core');
+const axios = require('axios');
 
 const client = new Discord.Client();
 client.login(`${process.env.BOT_TOKEN}`);
